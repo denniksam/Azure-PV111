214454c0f94b68fb294c3fc70cf73f57bff990f0
Denys Samoilenko <denniksam@gmail.com>
2023-11-15 18:05:16 +0200

    Add project files.

---
+++ Middleware/DataMiddleware.cs
@@ -0,0 +1,17 @@
+﻿namespace Azure_PV111.Middleware
+{
+    public class DataMiddleware
+    {
+        private readonly RequestDelegate _next;
+
+        public DataMiddleware(RequestDelegate next)
+        {
+            _next = next;
+        }
+
+        public async Task InvokeAsync(HttpContext context)
+        {
+            await _next(context);
+        }
+    }
+}
---
+++ Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace Azure_PV111.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
