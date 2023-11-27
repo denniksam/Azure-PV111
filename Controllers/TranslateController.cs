935a764c0f2081f8942730735faf599e1be752d8
Denys Samoilenko <denniksam@gmail.com>
2023-11-27 19:34:03 +0200

    Transliter

--- Controllers/TranslateController.cs
+++ Controllers/TranslateController.cs
@@ -11,10 +11,12 @@ namespace Azure_PV111.Controllers
     public class TranslateController : ControllerBase
     {
         private readonly IConfiguration _configuration;
+        private readonly ILogger<TranslateController> _logger;
 
-        public TranslateController(IConfiguration configuration)
+        public TranslateController(IConfiguration configuration, ILogger<TranslateController> logger)
         {
             _configuration = configuration;
+            _logger = logger;
         }
 
         [HttpGet]
@@ -46,6 +48,45 @@ namespace Azure_PV111.Controllers
                 string result = await response.Content.ReadAsStringAsync();
                 return result;
                 
+            }
+            else return new { Status = "Error" };
+        }
+
+        [HttpPost]
+        public async Task<object> PostAsync(
+            [FromQuery] String text, 
+            [FromQuery] String from, 
+            [FromQuery] String fromScript, 
+            [FromQuery] String toScript)
+        {
+            String? endpoint = _configuration.GetSection("Translator").GetSection("Endpoint").Value;
+            String? key = _configuration.GetSection("Translator").GetSection("Key").Value;
+            String? location = _configuration.GetSection("Translator").GetSection("Location").Value;
+            if (endpoint != null && key != null && location != null)
+            {
+                endpoint += $"/transliterate?api-version=3.0&language={from}&fromScript={fromScript}&toScript={toScript}";
+                _logger.LogInformation("PostAsync request: {endpoint}", endpoint);
+
+                object[] body = new object[] { new { Text = text } };
+                var requestBody = JsonSerializer.Serialize(body);
+                // language fromScript toScript
+                using var client = new HttpClient();
+                using var request = new HttpRequestMessage();
+
+                // Build the request.
+                request.Method = HttpMethod.Post;
+                request.RequestUri = new Uri(endpoint);
+                request.Content = new StringContent(requestBody, Encoding.UTF8, "application/json");
+                request.Headers.Add("Ocp-Apim-Subscription-Key", key);
+                // location required if you're using a multi-service or regional (not global) resource.
+                request.Headers.Add("Ocp-Apim-Subscription-Region", location);
+
+                // Send the request and get response.
+                HttpResponseMessage response = await client.SendAsync(request).ConfigureAwait(false);
+                // Read response as a string.
+                string result = await response.Content.ReadAsStringAsync();
+                return result;
+
             }
             else return new { Status = "Error" };
         }
--- wwwroot/js/site.js
+++ wwwroot/js/site.js
@@ -1,10 +1,14 @@
 ﻿document.addEventListener('DOMContentLoaded', function () {
     const translateButton = document.getElementById("translator-translate");
     if (translateButton) translateButton.addEventListener('click', translateClick);
+
+    const transliterateButton = document.getElementById("translator-transliterate");
+    if (transliterateButton) transliterateButton.addEventListener('click', transliterateClick);
+
+
 });
 
-
-function translateClick() {
+function getTranslateData() {
     const input = document.getElementById("translator-input");
     if (!input) throw "#translator-input not found";
     const text = input.value.trim();
@@ -17,11 +21,16 @@ function translateClick() {
     if (!output) throw "#translator-output not found";
 
     const langFrom = document.getElementById("translator-lang-from");
-    if (!langFrom) throw "#translator-lang-from not found";    
+    if (!langFrom) throw "#translator-lang-from not found";
 
     const langTo = document.getElementById("translator-lang-to");
     if (!langTo) throw "#translator-lang-to not found";
 
+    return [text, langFrom, langTo, output];
+}
+
+function translateClick() {    
+    let [text, langFrom, langTo, output] = getTranslateData();
     fetch(`/api/translate?text=${text}&from=${langFrom.value}&to=${langTo.value}`)
         .then(r => r.json())
         .then(j => {
@@ -32,3 +41,28 @@ function translateClick() {
     // output.value = input.value + ' ' + langFrom.value + ' ' + langTo.value;
     // "translator-switch" "translator-lang-from" "translator-lang-to"
 }  
+
+function transliterateClick() {
+    let [_, __, langTo, output] = getTranslateData();
+    const text = output.value;
+    if (!text) {
+        alert("Спочатку виконайте переклад");
+        return;
+    }
+    const fromScript = langTo.selectedOptions[0].getAttribute("data-script");
+    if (!fromScript) {
+        alert("Транслітерація цієї мови не підтримується");
+        return;
+    }
+    const toScript = "Latn";
+
+    fetch(
+        `/api/translate?text=${text}&from=${langTo.value}&fromScript=${fromScript}&toScript=${toScript}`, {
+            method: "POST"
+        })
+        .then(r => r.text())
+        .then(j => {
+            console.log(j);
+            // output.value = j[0].translations[0].text;
+        });
+}
